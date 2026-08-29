8893ebc9de82eaacc9b2f81843535d3f0af67581
siprasmabar <towonderwild@gmail.com>
2026-08-29 09:17:58 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'sipras-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch/Load dari Cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return file dari cache jika ada, jika tidak ambil dari internet
+        return response || fetch(event.request);
+      })
+  );
+});
