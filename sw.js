5da30cb9058aa29f2e4aa4f320396fec9226fdb7
siprasmabar <towonderwild@gmail.com>
2026-08-29 09:30:02 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 const CACHE_NAME = 'sipras-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
 // Install Service Worker
